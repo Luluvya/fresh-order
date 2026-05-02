7330be68afa398adb04e390181532a56eecc5ad8
DanlanZhai <97397760+Luluvya@users.noreply.github.com>
2026-05-02 21:56:20 +0800

    Create constants.php

---
+++ config/constants.php
@@ -0,0 +1,17 @@
+<?php
+    //start session
+    session_start();
+
+
+
+
+        //create constants to store non repeating values
+        define('SITEURL','http://localhost:88/fresh-order/');
+        define('LOCALHOST','localhost');
+        define('DB_USERNAME','root');
+        define('DB_PASSWORD','');
+        define('DB_NAME','fresh-order');
+        
+        $conn = mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD) or die(mysqli_error());//Database Connection
+        $db_select=mysqli_select_db($conn,DB_NAME)or die(mysqli_error());//Selecting Database
+?>
